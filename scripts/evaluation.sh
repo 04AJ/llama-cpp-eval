5b58d29c47bfa82b37cbbb79b11c7779dd69bb6b
04AJ <alanjcd2004@gmail.com>
2026-03-04 16:01:40 -0600

    eval results

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=build
+#SBATCH --job-name=evaluation
 #SBATCH --account=commons
 #SBATCH --partition=commons
 #SBATCH --reservation=classroom
 #SBATCH --ntasks=1 
-#SBATCH --output=logs/build_%j.log
-#SBATCH --error=logs/build_%j.err
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=4G
-#SBATCH --gres=gpu:volta:1
+#SBATCH --output=logs/eval_%j.log
+#SBATCH --error=logs/eval_%j.err
+#SBATCH --cpus-per-task=80   
+#SBATCH --mem=32G
 #SBATCH --time=00:45:00
 
 # Load modules
+module load CUDA
 module load GCCcore/14.3.0       
 module load git/2.50.1
 
@@ -21,7 +21,7 @@ mkdir -p $BASE_DIR/cache/llama.cpp
 mkdir -p "${BASE_DIR}/evaluation"
 
 PROJECT_DIR="${BASE_DIR}/src/llama.cpp"
-
+EVAL_PROJECT_DIR="$HOME/llama-cpp-eval"  
 
 # Environment variables
 export LLAMA_CACHE="$BASE_DIR/cache/llama.cpp"
@@ -29,14 +29,33 @@ export HF_HOME="$BASE_DIR/cache/llama.cpp"
 
 # Configure the build
 cd $PROJECT_DIR
+# rm -rf build
+
 cmake -B build
 
 # Compile using all 8 CPU cores
 cmake --build build --config Release -j 8
 
-# Obtaining and quantizing the model
+# Download models for evaluation
+MODEL_URL="https://huggingface.co/bartowski/Llama-3.2-1B-Instruct-GGUF/resolve/main/Llama-3.2-1B-Instruct-Q4_K_M.gguf"
+MODEL_FILE="${BASE_DIR}/cache/llama.cpp/Llama-3.2-1B-Instruct-Q4_K_M.gguf"
+
+# Download only if the file doesn't exist
+if [ ! -f "$MODEL_FILE" ]; then
+    echo "Downloading model..."
+    wget -c "$MODEL_URL" -O "$MODEL_FILE"
+else
+    echo "Model already exists, skipping download."
+fi
+
+
+# llama-bench can perform three types of tests:
+
+# Prompt processing (pp): processing a prompt in batches (-p)
+# Text generation (tg): generating a sequence of tokens (-n)
+# Prompt processing + text generation (pg): processing a prompt followed by generating a sequence of tokens (-pg)
+
 cd build/bin/
+./llama-bench -m "$MODEL_FILE" -t 8,16,32,40,48,64,80 -p 512 -n 128 -o csv > "${EVAL_PROJECT_DIR}/evaluation/recreate_bug_${SLURM_JOB_ID}.csv"
 
-
-# Start bench mark
-./llama-bench -n 0 -p 1024 -b 128,256,512,1024 -o csv > "${BASE_DIR}/evaluation/results_${SLURM_JOB_ID}.csv"
+echo "Benchmark complete. Results saved to: ${EVAL_PROJECT_DIR}/thread-bug_${SLURM_JOB_ID}.csv"
